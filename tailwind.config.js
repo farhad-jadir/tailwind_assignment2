0ef8a384c7556e83a9a864352074d908f069f3e9
farhadjony <farhadjony.math@gmail.com>
2024-10-01 19:13:43 +0600

    tailwind2 done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/image/edwin-andrade-4V1dC_eoCwg-unsplash 1.png')",
+        'footer-texture': "url('image')",
+      }
+    },
+  },
+  plugins: [],
+}
+
